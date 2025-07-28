c4aba2911a0ddfe8903ed51b901d3f721e0c2aba
SamT123 <samturner0306@gmail.com>
2025-07-28 14:01:03 +0100

    update make_cmaple_tree.R

--- R/make_tree_functions.R
+++ R/make_tree_functions.R
@@ -4,18 +4,21 @@
 #' @param tree_path path to save tree
 #' @param starting_tree_path path to starting tree containing a subset of the sequences
 #' @param freeze_starting_tree should the relationships in the starting tree be preserved?
+#' @param cmaple_path path to your cmaple installation
+#' @param out_sequence name of outgroup for tree rooting
 #'
 #'@export
 make_cmaple_tree = function(
   sequences,
   tree_path,
   starting_tree_path = NULL,
-  freeze_starting_tree = F
+  freeze_starting_tree = F,
+  cmaple_path = NULL,
+  out_sequence = NULL
 ) {
-  fasta_path = paste0(
-    tools::file_path_sans_ext(tree_path),
-    ".fasta"
-  )
+  if (!is.null(cmaple_path)) {
+    add_to_PATH(cmaple_path)
+  }
 
   if (!is.null(starting_tree_path)) {
     starting_tree = castor::read_tree(file = starting_tree_path)
@@ -34,10 +37,7 @@ make_cmaple_tree = function(
     )
   }
 
-  old_path <- Sys.getenv("PATH")
-  Sys.setenv(
-    PATH = paste(old_path, "~/miniforge3/envs/treebuild/bin/", sep = ":")
-  )
+  fasta_path = fs::path_ext_set(tree_path, ".fasta")
 
   seqUtils::write_fast_fasta(
     sequences,
@@ -45,40 +45,68 @@ make_cmaple_tree = function(
     path = fasta_path
   )
 
+  # fmt: skip
+  cmaple_call = c(
+    "cmaple",
+    "-aln", fasta_path
+  )
+
   if (!is.null(starting_tree_path)) {
-    if (!freeze_starting_tree) {
-      system(
-        paste0(
-          "cmaple -aln ",
-          fasta_path,
-          " -t ",
-          starting_tree_path,
-          " -search EXHAUSTIVE -nt AUTO -m GTR"
-        )
-      )
-    } else {
-      system(
-        paste0(
-          "cmaple -aln ",
-          fasta_path,
-          " -t ",
-          starting_tree_path,
-          " -nt AUTO -m GTR"
-        )
-      )
-    }
-  } else {
-    system(
-      paste0(
-        "cmaple -aln ",
-        fasta_path,
-        " -search EXHAUSTIVE -nt AUTO -m GTR"
-      )
+    # fmt: skip
+    cmaple_call = c(
+      cmaple_call,
+      " -t ", starting_tree_path
     )
   }
+
+  if (!freeze_starting_tree) {
+    # fmt: skip
+    cmaple_call = c(
+      cmaple_call,
+      "-search", "EXHAUSTIVE"
+    )
+  } else {
+    # fmt: skip
+    cmaple_call = c(
+      cmaple_call,
+      "-search", "NORMAL"
+    )
+  }
+
+  # fmt: skip
+  cmaple_call = c(
+      cmaple_call,
+      "-nt", "AUTO",
+      "-m", "GTR"
+    )
+
+  system(paste(cmaple_call, collapse = " "))
 
   system(paste0("mv ", fasta_path, ".treefile ", tree_path))
   system(paste0("mv ", fasta_path, ".log ", tree_path, ".log"))
 
+  tree = castor::read_tree(file = tree_path)
+  ladderizeAndMaybeRoot(tree, out_sequence)
+  castor::write_tree(tree, tree_path)
+
   tree_path
+}
+
+
+ladderizeAndMaybeRoot = function(tree, out_sequence = NULL) {
+  if (!is.null(out_sequence)) {
+    stopifnot(out_sequence %in% tree$tip.label)
+
+    tree = tree %>%
+      ape::unroot() %>%
+      ape::root(outgroup = out_sequence)
+  }
+
+  tree = ape::ladderize(tree)
+
+  f = fs::file_temp()
+  castor::write_tree(tree, file = f)
+  tree_read = castor::read_tree(file = f)
+
+  tree_read
 }
